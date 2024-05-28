9850ee066f79f4918b307feaf54ffc16284f5360
Mamun <mamunslogic@hotmail.com>
2024-05-29 00:16:36 +0600

    First Upload

---
+++ DesignPatternImplementation/Program.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatternImplementation;
+
+//--------  For single thread ------------
+//var firstInstance = Singleton.GetInstance();
+//firstInstance.Print("Print from first instance.");
+
+//var secondInstance = Singleton.GetInstance();
+//secondInstance.Print("Print from second instance.");
+
+
+
+//----------------- For multiple thread ------------------
+Parallel.Invoke
+    (
+                () => SingletonThreadSafe.GetInstance().Print("Print from first instance."),
+                () => SingletonThreadSafe.GetInstance().Print("Print from second instance.")
+    );
+
+SingletonThreadSafe.PrintCounter();
+Console.ReadLine();
---
+++ DesignPatternImplementation/SingletonDesignPatternThreadSafe.cs
@@ -0,0 +1,55 @@
+﻿namespace DesignPatternImplementation
+{
+    // 1. Make thread safe - using Lock
+    // 2. Make thread safe - using Double-Check Locking
+
+    public sealed class SingletonThreadSafe
+    {
+        private static int _counter = 0;
+        private static SingletonThreadSafe _instance = null;
+
+        private static readonly object instanceLock = new();
+
+        public static SingletonThreadSafe GetInstance()
+        {
+            //lock (instanceLock)
+            //{
+            //    if (_instance == null)
+            //    {
+            //        _instance = new SingletonThreadSafe();
+            //        _counter++;
+            //    }
+            //}
+
+            if (_instance == null)
+            {
+                lock (instanceLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonThreadSafe();
+                        _counter++;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        private SingletonThreadSafe()
+        {
+            //_counter++;
+            //Console.WriteLine("Counter Value " + _counter.ToString());
+        }
+
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void PrintCounter()
+        {
+            Console.WriteLine("Counter Value:" + _counter.ToString());
+        }
+    }
+}
