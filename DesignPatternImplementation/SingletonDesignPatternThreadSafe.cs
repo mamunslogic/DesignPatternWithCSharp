a05481055ec81bff85392eb89a28f99bbd0a8966
Mamun <mamunslogic@hotmail.com>
2024-05-29 23:46:22 +0600

    Eager Loading and Lazy Loading is implemented

--- DesignPatternImplementation/SingletonDesignPatternThreadSafe.cs
+++ DesignPatternImplementation/SingletonDesignPatternThreadSafe.cs
@@ -3,16 +3,22 @@
     //-------------- Singleton in thread safe ----------------------------
     // 1. Make thread safe - using Lock
     // 2. Make thread safe - using Double-Check Locking
+    // 3. Make thread safe - using Eager Loading or Non-Lazy
+    // 4. Make thread safe - using Lazy Loading or Deferred Loading means using Lazy<T>
 
     public sealed class SingletonThreadSafe
     {
         private static int _counter = 0;
-        private static SingletonThreadSafe _instance = null;
+        //private static SingletonThreadSafe _instance = null;
+        //private static readonly object instanceLock = new();
 
-        private static readonly object instanceLock = new();
+        //private static readonly SingletonThreadSafe _instance = new();
+
+        private static readonly Lazy<SingletonThreadSafe> _instance = new(() => new SingletonThreadSafe());
 
         public static SingletonThreadSafe GetInstance()
         {
+            // -------------- Thread safe using lock ---------------
             //lock (instanceLock)
             //{
             //    if (_instance == null)
@@ -22,24 +28,26 @@
             //    }
             //}
 
-            if (_instance == null)
-            {
-                lock (instanceLock)
-                {
-                    if (_instance == null)
-                    {
-                        _instance = new SingletonThreadSafe();
-                        _counter++;
-                    }
-                }
-            }
 
-            return _instance;
+            // -------------- Thread safe using double check lock ---------------
+            //if (_instance == null)
+            //{
+            //    lock (instanceLock)
+            //    {
+            //        if (_instance == null)
+            //        {
+            //            _instance = new SingletonThreadSafe();
+            //            _counter++;
+            //        }
+            //    }
+            //}
+
+            return _instance.Value;
         }
 
         private SingletonThreadSafe()
         {
-            //_counter++;
+            _counter++;
             //Console.WriteLine("Counter Value " + _counter.ToString());
         }
 
