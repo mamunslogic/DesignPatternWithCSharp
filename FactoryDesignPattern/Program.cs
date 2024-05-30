e02426c6215fd4321754105be7b5cb8078fe174a
Mamun <mamunslogic@hotmail.com>
2024-05-31 00:34:06 +0600

    Narration added to Factory Design Pattern

--- FactoryDesignPattern/Program.cs
+++ FactoryDesignPattern/Program.cs
@@ -1,4 +1,10 @@
-﻿using FactoryDesignPattern;
+﻿
+    //As per the definition of Factory Design Pattern, 
+    //1. the Factory Design Pattern creates an object without exposing the object creation logic to the client, 
+    //2. and the client refers to the newly created object using a common interface.
+
+
+using FactoryDesignPattern;
 
 var emailNotificationSender = NotificationFactory.GetNotificationSender(NotificationSenderType.Email);
 emailNotificationSender.SendNotification("This is an email notification.");
